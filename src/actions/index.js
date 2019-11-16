a51025b2240ac4a9c512f7a8ad1565d9091df6ad
modeverv <modeverv@gmail.com>
2019-11-17 01:11:35 +0900

    springから配信を試みる

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const ROOT_URL = 'https://tw2bkend.lovesaemi.daemon.asia/'
+const ROOT_URL = '/'
 const TAGS = 'tags'
 const FAVOS = 'favos'
 export const READ_TAGS = "READ_TAGS"
@@ -8,19 +8,25 @@ export const READ_FAVOS = "READ_FAVOS"
 export const readFavos = values => async dispatch => {
     console.log(values)
     var ar_qs = []
-    if(values.tag) {
+    if (values.tag) {
         ar_qs.push("tag=" + encodeURI(values.tag))
     }
-    if(values.offset) {
+    if (values.offset) {
         ar_qs.push("offset=" + values.offset * 10)
     } else {
         ar_qs.push("offset=0")
     }
     const qs = ar_qs.join("&")
     const response = await axios.get(`${ROOT_URL}/${FAVOS}?${qs}`)
-    dispatch({type: READ_FAVOS, response})
+    dispatch({
+        type: READ_FAVOS,
+        response
+    })
 }
 export const readTags = () => async dispatch => {
     const response = await axios.get(`${ROOT_URL}/${TAGS}`)
-    dispatch({type: READ_TAGS, response})
+    dispatch({
+        type: READ_TAGS,
+        response
+    })
 }
